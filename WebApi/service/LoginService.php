d8ed0b1d6c85c754d03312f3e5909a09add3afd0
Eduardo Gusqui <egusqui@vimeworks.com>
2018-09-04 17:00:31 -0500

    Refactor metodo autenticacion

--- WebApi/service/LoginService.php
+++ WebApi/service/LoginService.php
@@ -6,26 +6,32 @@ require_once("WebApi/service/AuthService.php");
 class LoginService {
 
 	public static function authenticate($data) {
-		$db = Database::getConnection();
-		$credentials = json_decode($data);
-		$sql = "select u.id, u.nombre_usuario, r.id as idRol
+		try {
+			$db = Database::getConnection();
+			$credentials = json_decode($data);
+			$sql = "select u.id, u.nombre_usuario, r.id as idRol
 					from usuario u
 					left join rol r on r.id = u.idRol
 					where u.nombre_usuario = ?
 					and u.contrasenia = ?";
-		$result = $db->execute($sql, array($credentials->Username,md5($credentials->Password)));
-		if (!is_null($result)) {
-			$authenticate = new AuthenticateModel();
-			$authenticate->Id = $result->id;
-			$authenticate->Username = $result->nombre_usuario;
-			$authenticate->IdRol = $result->idRol;
-			$token = AuthService::SignIn([
-				'id' => $authenticate->Id,
-				'name' => $authenticate->Username
-			]);
-			$authenticate->Token = $token;
+			$result = $db->execute($sql, array($credentials->Username,md5($credentials->Password)));
+			if (!is_null($result)) {
+				$authenticate = new AuthenticateModel();
+				$authenticate->Id = $result->id;
+				$authenticate->Username = $result->nombre_usuario;
+				$authenticate->IdRol = $result->idRol;
+				$token = AuthService::SignIn([
+					'id' => $authenticate->Id,
+					'name' => $authenticate->Username
+				]);
+				$authenticate->Token = $token;
 
-			return $authenticate;
+				return $authenticate;
+			} else {
+				return null;
+			}
+		} catch (Exception $e) {
+			throw new Exception($e->getMessage());
 		}
 	}
 }
