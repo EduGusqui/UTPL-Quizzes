d94de7fa755ca16779cfce7b9739c42a56235b67
Eduardo Gusqui <egusqui@vimeworks.com>
2018-09-08 22:21:05 -0500

    Refactor del modulo para inyectar los componentes

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -1,38 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule }   from '@angular/forms';
+import { DataTablesModule } from 'angular-datatables';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from './helpers/jwt.interceptor';
+import { ErrorInterceptor } from './helpers/error.interceptor';
+import { LoginModule } from './login/login.module';
+import { AppConfig } from './app.config';
+import { MainViewModule } from './main/main.module';
+import { UserModule } from './users/user.module';
 import { NavigationComponent } from './layout/navigation.component';
-import { AppHeaderComponent } from './layout/header.component';
-import { AppMenuComponent } from './layout/menu.component';
-import { AppFooterComponent } from './layout/footer.component';
+import { MenuComponent } from './layout/menu.component';
+import { HeaderComponent } from './layout/header.component';
+import { FooterComponent } from './layout/footer.component';
+import { appRoutingProviders, routing } from './app-routing.module';
+import { AssignQuizModule } from './assignQuiz/assignQuiz.module';
+import { TakeQuizModule } from './takeQuiz/takeQuiz.module';
 
-import { UserListComponent } from './components/user/list.component';
-import { UserComponent } from './components/user/form.component';
-import { ModalComponent } from './components/modal/modal.component';
+/*export function appConfigServiceFactory(appConfig: AppConfig) {
+  return () => appConfig.load();
+};*/
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavigationComponent,
-    AppHeaderComponent,
-    AppMenuComponent,
-    AppFooterComponent,
-    UserComponent,
-    UserListComponent,
-    ModalComponent
+    AppComponent, NavigationComponent, MenuComponent, HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule
+    BrowserModule, HttpClientModule, FormsModule, DataTablesModule, routing, LoginModule,
+    MainViewModule, UserModule, AssignQuizModule, TakeQuizModule
   ],
-  providers: [],
+  providers: [ appRoutingProviders,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    //{ provide: APP_INITIALIZER, useFactory: appConfigServiceFactory, deps: [AppConfig], multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
