7465801d5673e70f5de03056ad7d3fb64829b0ff
Eduardo Gusqui <egusqui@vimeworks.com>
2018-09-08 22:27:44 -0500

    Se agrega el codigo para activar las rutas de acuerdo al rol

--- WebApp/src/app/layout/menu.component.ts
+++ WebApp/src/app/layout/menu.component.ts
@@ -1,18 +1,48 @@
-import { Component, OnInit, ElementRef} from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionService } from '../utils/session.service';
+import { Constants } from '../utils/constants';
+
+declare var jQuery:any;
 
 @Component({
 	selector: 'app-menu',
 	templateUrl: 'menu.component.html'
 })
 
-export class AppMenuComponent implements OnInit {
-	constructor(private route: ActivatedRoute, private _router: Router,
-		private elementRef:ElementRef) { }
+export class MenuComponent {
+	constructor(private router: Router, public sessionService: SessionService, private constants: Constants) {
+	}
 
-	ngOnInit(){ }
+	ngAfterViewInit() {
+		jQuery('#side-menu').metisMenu();
+	}
 
 	activeRoute(path: string): boolean {
-		return true;
+		if (path === this.constants.ROL_ADMIN) {
+			if (this.router.url === '/users') {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		if (path === this.constants.ROL_TEACHER) {
+			if (this.router.url === '/quizzes') {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		if (path === this.constants.ROL_STUDENT) {
+			if (this.router.url === '/takesQuizzes') {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		return false;
 	}
-} 
+}
