03a061e75f9362d21515f79c66e157d81c03b62c
Eduardo Gusqui <egusqui@vimeworks.com>
2018-09-08 22:41:53 -0500

    Se agrega el modulo y componente de pagina de inicio de la aplicación

---
+++ WebApp/src/app/main/main.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { MainViewComponent } from './main.component';
+
+@NgModule({
+    declarations: [MainViewComponent],
+    imports     : [BrowserModule],
+})
+
+export class MainViewModule {}
